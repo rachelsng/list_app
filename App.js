c6674b671fe3c2738f05949e710379ab5522b0d6
galaxydefender93 <rachelsngweilin@gmail.com>
2023-10-09 21:23:29 +0800

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Text, View } from "react-native";
+import {names} from './data/names.js'
+import {styles} from './data/styles.js'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+	<View style={styles.container}>
+      {names.map((name, index) => (
+        <Text key={index}>{name}</Text>
+      ))}
+  	<StatusBar style="auto" />
+	</View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+;
